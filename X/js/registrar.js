c9adb42686e52fffebacfe19f5edc73fb28954cf
Naydelin Zavala <150888285+Nayde666@users.noreply.github.com>
2023-11-23 10:22:37 -0600

    tabiennn
    tabien

--- X/js/registrar.js
+++ X/js/registrar.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const url = window.location.search
     const params = new URLSearchParams(url)
     const existe = params.get('existe')
-    if(existe){
-      const alerta = document.getElementsByClassName('alert')
-      alerta[0].classList.remove('hide')
-      alerta[0].classList.add('show')
-      setTimeout(() => {
-        alerta[0].classList.remove('show')
-        alerta[0].classList.add('hide')
-      }, 3000)
+    if(existe) {
+        const alerta = document.getElementsByClassName('alert')
+        alerta[0].classList.remove('hide')
+        alerta[0].classList.add('show')
+        setTimeout(() => {
+            alerta[0].classList.remove('show')
+            alerta[0].classList.add('hide')
+        }, 3000);
     }
-    console.log('@@@ params => ', params, existe)
-  })
+    //console.log(params, existe)
+})
