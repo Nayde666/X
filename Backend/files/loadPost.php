ca9cb8a21912fbf68dd26c04d5de2def087eacac
Naydelin Zavala <150888285+Nayde666@users.noreply.github.com>
2023-11-28 17:30:57 -0600

    funcional solo falta diseño
    funcional solo falta diseño

--- Backend/files/loadPost.php
+++ Backend/files/loadPost.php
@@ -1,23 +1,10 @@
 <?php
-/*  include("../config/conexion.php");
-  $conn = conectar();
-  $querySelect = "SELECT * FROM posts";
-  $variableUsuario = mysqli_query($conn, $querySelect);
-  $posts = mysqli_query($conn, $querySelect);
-  $postsArray = [];
-  if($posts->num_rows >0){
-    while($post = mysqli_fetch_array($posts)){
-      $postsArray[] = $post;
-    }
-    echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => $postsArray]);
-  } else {
-    echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'No existen posts']);
-  }*/
   include("../config/conexion.php");
   $conn = conectar();
   $querySelect = "SELECT posts.*, COUNT(comentario.idComentario) as totalComentarios FROM posts LEFT JOIN comentario ON posts.idPost = comentario.idPost GROUP BY posts.idPost";
   $posts = mysqli_query($conn, $querySelect);
   $postsArray = [];
+  
   if($posts->num_rows >0){
     while($post = mysqli_fetch_array($posts)){
       $postsArray[] = $post;
