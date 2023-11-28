3472db1bba4b347f9ad6920e46cc3a369d329292
Naydelin Zavala <150888285+Nayde666@users.noreply.github.com>
2023-11-28 17:33:23 -0600

    funcional
    funcional

--- js/home.js
+++ js/home.js
@@ -3,6 +3,7 @@ let loggedUser = {}
 const titulo = document.getElementById('userBlog')
 const nombre = document.getElementById('userName')
 const postContainer = document.getElementById('postUsuarios')
+
 const postCard = document.getElementById('cardPost').content
 const fragment = document.createDocumentFragment()
 
@@ -13,25 +14,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 postContainer.addEventListener('click', async (e) => {
     const btnComment = e.target.closest('.btn-comment')
+    
     if (btnComment) {
       const idPost = btnComment.getAttribute('data-idpost')
       document.getElementById('commentPostId').value = idPost
-      
-      /*const mensaje = btnComment.getAttribute('data-mensaje')
-      document.getElementById('commentText').value = mensaje*/
-
-
-
+    
 	  const idUsuarioComment = document.getElementById('idUsuario-Comment')
 	  idUsuarioComment.value = loggedUser.usuario
 
       const inputnombre = document.getElementById('nombreUsuario-Comment')
       inputnombre.value = loggedUser.nombre
 
-	  //console.log('ids => ', idPost, mensaje, loggedUser.usuario)
+    }
 
-      const commentModal = new bootstrap.Modal(document.getElementById('commentModal'))
-      commentModal.show()
+    const buttonloadComment = e.target.closest('.buttonloadComment')
+    if (buttonloadComment){
+        const idPost = buttonloadComment.getAttribute('data-idpost')
+        const urlParams = new URLSearchParams(window.location.search);
+        const usuario = urlParams.get('usuario');
+        const nuevaURL = `/x/loadcomment.html?usuario=${usuario}&idPost=${idPost}`;
+        window.location.replace(nuevaURL);
     }
 })
 
@@ -51,21 +53,28 @@ const dibujaPosts = (posts)=> {
         postCard.querySelector('.card-text').textContent = item.mensaje
         postCard.querySelector('.fecha').textContent = item.fecha
 
-        const comentariosParrafo = postCard.querySelector('.comentarios-parrafo')
-        comentariosParrafo.textContent = `Comentarios: ${item.totalComentarios}`
-
         const posttexto= postCard.querySelector('.contenidodelpost')
         posttexto.textContent = item.mensaje
+
         const postnombre= postCard.querySelector('.nombreusuariopost')
         postnombre.textContent = item.nombre
+
         const postidusuario= postCard.querySelector('.idusuariopost')
         postidusuario.textContent = item.idUsuario
 
+        const comentariosParrafo = postCard.querySelector('.comentarios-parrafo')
+        comentariosParrafo.textContent = `Comentarios: ${item.totalComentarios}`
         const clone = postCard.cloneNode(true)
+
         const btnComment = clone.querySelector('.btn-comment')
         btnComment.setAttribute('data-idpost', item.idPost)
         btnComment.setAttribute('data-mensaje', item.mensaje)
-
+        
+        const buttonloadComment = clone.querySelector('.buttonloadComment')
+        if(buttonloadComment){
+            buttonloadComment.setAttribute('data-idpost', item.idPost)
+        }
+        
         fragment.appendChild(clone)
     })
     postContainer.appendChild(fragment)
@@ -103,10 +112,5 @@ const loadUser  = () => {
     console.log('@@@ usuario => ', usuario)
 }
 
-let post
-const btnPost = document.getElementById('postUsuarios')
 
-btnPost.addEventListener('click', () => {
-    
-})
 
